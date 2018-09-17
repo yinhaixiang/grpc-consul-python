1e2c2eae6c6f65326fbfb6655cc4c742f8bb8c7a
yin.hx <yin.hx@burgeon.cn>
2018-09-17 21:41:10 +0800

    '增加consul'

--- client.py
+++ client.py
@@ -5,12 +5,12 @@ from __future__ import print_function
 import grpc
 from proto import hello_pb2, hello_pb2_grpc
 
-_HOST = 'localhost'
-_PORT = '8188'
+_HOST = '127.0.0.1'
+_PORT = 8188
 
 
 def run():
-    channel = grpc.insecure_channel(_HOST + ':' + _PORT)
+    channel = grpc.insecure_channel(_HOST + ':' + str(_PORT))
     client = hello_pb2_grpc.GrpcActionStub(channel=channel)
 
     name = '哈哈22sean哈哈'
--- server_consul.py
+++ server_consul.py
@@ -1,33 +1,58 @@
 # -*- coding: utf-8 -*-
+import consul
 import grpc
 import time
-import consul
+import json
 from concurrent import futures
 from proto import hello_pb2, hello_pb2_grpc
 
-_HOST = 'localhost'
-_PORT = '8188'
+_HOST = '127.0.0.1'
+_PORT = 8188
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
-class gRPCServicerImpl(hello_pb2_grpc.GrpcActionServicer):
+class GrpcActionServicerImpl(hello_pb2_grpc.GrpcActionServicer):
 
     def SayHello(self, request, context):
         print("called with " + request.name)
         return hello_pb2.HelloReply(message='Hello, %s!' % request.name)
 
 
+def register():
+    print("register started...")
+    c = consul.Consul()  # 连接consul 服务器，默认是127.0.0.1，可用host参数指定host
+    check = consul.Check.tcp(_HOST, _PORT, "30s")  # 健康检查的ip，端口，检查时间
+
+    c.agent.service.register(
+        "search-service",
+        f"search-service-{_PORT}",
+        address=_HOST,
+        port=_PORT,
+        check=check
+    )  # 注册服务部分
+    print("注册服务search-service成功")
+    print("services: " + str(c.agent.services()))
+
+
+def unregister():
+    print("unregister started")
+    c = consul.Consul()
+    c.agent.service.deregister(f"search-service-{_PORT}")
+
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    hello_pb2_grpc.add_GrpcActionServicer_to_server(gRPCServicerImpl(), server)
-    server.add_insecure_port('[::]:' + _PORT)
+    hello_pb2_grpc.add_GrpcActionServicer_to_server(GrpcActionServicerImpl(), server)
+    server.add_insecure_port('[::]:' + str(_PORT))
+    register()
     server.start()
     print('server start success')
     try:
         while True:
             time.sleep(_ONE_DAY_IN_SECONDS)
     except KeyboardInterrupt:
+        unregister()
         server.stop(0)
 
 
